1b9a8d3314e208f11d17f9ab7220c5e707da2327
Piyush Keshari <piyushkeshari24@gmail.com>
2024-11-20 01:58:44 +0530

    converted to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+            manifest: {
+                name: 'Meditation Timer',
+                short_name: 'MedTimer',
+                description: 'A peaceful meditation timer application',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     base: '/FlowBeat/',
     server: {
         port: 5173,
